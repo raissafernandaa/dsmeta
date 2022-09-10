6738e698163863465e57252b64ee76dc36eebeca
raissafernandaa <raissa.fernanda93@hotmail.com>
2022-09-10 16:01:56 +0200

    Other components

---
+++ frontend/src/assets/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from '../../img/logo.svg'
+import './styles.css'
+
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta"/>
+                    <h1>DSMeta</h1>
+                    <p>
+                        Desenvolvido por
+                        <a href="https://www.instagram.com/devsuperior.ig/?hl=pt">@devsuperior.ig</a>
+                    </p>
+            </div>
+        </header>
+
+        )
+    }
+
+export default Header
